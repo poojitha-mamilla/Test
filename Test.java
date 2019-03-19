dd6b468861886dffce7f05b9cd24f0cc9ef612c0
poojitha-mamilla <poojitha.mamilla29@gmail.com>
2019-03-19 15:47:09 +0530

    welcome

--- Test.java
+++ Test.java
@@ -1,6 +1,11 @@
+import java.util.*;
 class Test{
 public static void main(String args[]){
 	System.out.println("Hello world!");
+	Scanner sc=new Scanner(System.in);
+	String s=sc.next();
+	System.out.println(s+", Welcome");
+	
 }
 
 }
