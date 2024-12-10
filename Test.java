d4d3c06647c27686dfce470283fb7443c7c87cc9
poojitha-mamilla <47858605+poojitha-mamilla@users.noreply.github.com>
2024-12-10 23:00:57 +0530

    Test.java

--- Test.java
+++ Test.java
@@ -1,11 +1,6 @@
-import java.util.*;
 public class Test{
 public static void main(String args[]){
 	System.out.println("Hello world!");
-	/*Scanner sc=new Scanner(System.in);
-	String s=sc.next();
-	System.out.println(s+", Welcome");*/
 	
 }
-
 }
